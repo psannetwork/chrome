7431ccb3c7d068387882cec7e8526df18e1b72bf
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2025-02-20 17:46:56 +0900

    Update cloudflare-setup.sh

--- xfree-server/cloudflare-setup.sh
+++ xfree-server/cloudflare-setup.sh
@@ -1,1 +1,2 @@
 nohup cloudflared tunnel --no-autoupdate run --token eyJhIjoiZjM2NmY2MmEyZDFkYTJmMjM5MWVjY2MxZTY5ZWQ5MTQiLCJ0IjoiZTBmYTY3MjctYjVkMi00NzRmLTk5MmYtMzgwODA3OGRhZDgyIiwicyI6Ik5tRXpORFl5TUdJdE4yWmxZeTAwTTJNMExXRTVOV0V0WVRNM1pUZzJZVEEyTVRRMyJ9 --protocol http2 > /path/to/logfile 2>&1 &
+#ps aux | grep cloudflared
