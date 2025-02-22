6eff726510f2b15d6300525d8ac5a5901f033127
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2025-02-22 14:48:05 +0900

    Update cloudflare-setup.sh

--- xfree-server/cloudflare-setup.sh
+++ xfree-server/cloudflare-setup.sh
@@ -1,3 +1,3 @@
-nohup cloudflared tunnel --no-autoupdate run --token eyJhIjoiZjM2NmY2MmEyZDFkYTJmMjM5MWVjY2MxZTY5ZWQ5MTQiLCJ0IjoiZTBmYTY3MjctYjVkMi00NzRmLTk5MmYtMzgwODA3OGRhZDgyIiwicyI6Ik5tRXpORFl5TUdJdE4yWmxZeTAwTTJNMExXRTVOV0V0WVRNM1pUZzJZVEEyTVRRMyJ9 --protocol http2 > logfile 2>&1 &
+nohup cloudflared tunnel --no-autoupdate run --token token --protocol http2 > logfile 2>&1 &
 #ps aux | grep cloudflared
 #https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
