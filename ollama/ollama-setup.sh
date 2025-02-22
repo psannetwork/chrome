ea0f70285593738934c3e88b0767d4cb7c28931b
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2025-02-22 10:34:43 +0900

    Update ollama-setup.sh

--- ollama/ollama-setup.sh
+++ ollama/ollama-setup.sh
@@ -1,3 +1,4 @@
 wget https://github.com/ollama/ollama/releases/download/v0.5.11/ollama-linux-amd64.tgz
 mkdir -p ~/ollama
 tar -xvzf ollama-linux-amd64.tgz -C ~/ollama
+echo 'export PATH=$PATH:~/ollama' >> ~/.bashrc
