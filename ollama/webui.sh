d7650fa7f7de4c0aeeeceba8882075e2fa815734
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2025-03-09 21:04:51 +0900

    Create webui.sh

---
+++ ollama/webui.sh
@@ -0,0 +1,24 @@
+# pyenv をインストール
+apk add --no-cache --virtual .build-deps gcc make libssl-dev zlib-dev \
+    libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+    libncurses5-dev libncursesw5-dev xz-utils tk-dev \
+    libffi-dev liblzma-dev python3-dev git
+
+curl https://pyenv.run | bash
+
+# シェルの設定を更新 (bash の場合)
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+# Python 3.11 をインストール
+pyenv install 3.11.0
+pyenv global 3.11.0
+
+
+pip install open-webui
+
+open-webui serve &> ollama.log &
+ps aux | grep open-webui
+disown
