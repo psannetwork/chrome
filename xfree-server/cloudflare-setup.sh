8dcda9ecc547b1c0c54c903bab172d704739ef4b
psannetwork <127632545+hirotomoki12345@users.noreply.github.com>
2025-03-09 21:15:39 +0900

    Update cloudflare-setup.sh

--- xfree-server/cloudflare-setup.sh
+++ xfree-server/cloudflare-setup.sh
@@ -1,3 +1,6 @@
 nohup cloudflared tunnel --no-autoupdate run --token token --protocol http2 > logfile 2>&1 &
 #ps aux | grep cloudflared
 #https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+cloudflared tunnel --no-autoupdate run --token token --protocol http2 &> ollama.log &
+ps aux | grep open-webui
+disown
