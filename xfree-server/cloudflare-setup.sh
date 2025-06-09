c73f04f6a8af8998a0e0c2e168db57484c0bd0b0
psan <127632545+psannetwork@users.noreply.github.com>
2025-06-09 15:10:43 +0900

    Update cloudflare-setup.sh

--- xfree-server/cloudflare-setup.sh
+++ xfree-server/cloudflare-setup.sh
@@ -1,3 +1,3 @@
-nohup cloudflared tunnel --no-autoupdate run --token token --protocol http2 > logfile 2>&1 &
+nohup cloudflared tunnel --no-autoupdate run --token token --protocol http2 > /dev/null 2>&1 &
 #ps aux | grep cloudflared
 #https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
